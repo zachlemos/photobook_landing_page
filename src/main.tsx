c1dea4339cd2e0e6e2b7950a2bba118d74367ea0
zachlemos <zachlemos@gmail.com>
2025-07-08 22:29:47 -0700

    Remove all Clerk-related code from entrypoint for production deployment

--- src/main.tsx
+++ src/main.tsx
@@ -1,22 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { ClerkProvider } from "@clerk/clerk-react";
 import App from "./App";
 import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 
-const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
-console.log('VITE_CLERK_PUBLISHABLE_KEY:', PUBLISHABLE_KEY); // Debugging line
-if (!PUBLISHABLE_KEY) {
-  throw new Error("Missing Clerk Publishable Key");
-}
-
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </ClerkProvider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
